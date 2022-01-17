40b94c6f5b6bf2c99d43857dddb1292246f99873
SOUDAN Thomas <Thomas.Soudan@etu.univ-smb.fr>
2022-01-17 20:09:48 +0100

    item mais pas fini

---
+++ Marche/Item.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended;
+using MonoGame.Extended.ViewportAdapters;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended.Screens.Transitions;
+using System;
+
+namespace Marche
+{
+    public class Item
+    {
+        private string id;
+        private int level;
+        private int etat;
+        private Texture texture;
+
+        public Item(string id, int level, int etat, Texture texture)
+        {
+            this.id = id;
+            this.level = level;
+            this.etat = etat;
+            this.texture = texture;
+        }
+
+        public string Id
+        {
+            get
+            {
+                return this.id;
+            }
+
+            set
+            {
+
+                this.id = value;
+            }
+        }
+
+        public int Level
+        {
+            get
+            {
+                return this.level;
+            }
+
+            set
+            {
+                this.level = value;
+            }
+        }
+
+        public int Etat
+        {
+            get
+            {
+                return this.etat;
+            }
+
+            set
+            {
+                this.etat = value;
+            }
+        }
+
+        public Texture Texture
+        {
+            get
+            {
+                return this.texture;
+            }
+
+            set
+            {
+                this.texture = value;
+            }
+        }
+    }
+}
